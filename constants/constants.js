fd4a40903ca17cbea6f6dbc6157ca1d4bafd1e86
Amanda Bui <buixx199@umn.edu>
2021-07-19 17:28:10 -0500

    #1 Resolves pathing issue with crafting icon img

--- constants/constants.js
+++ constants/constants.js
@@ -3,4 +3,4 @@ export const INGREDIENTS = "Ingredients";
 export const CRAFT_INGREDIENTS = "Craft Ingredients";
 
 // IMAGE PATHS
-export const CRAFTING_ICON = "../images/craftingIcon.png";
+export const CRAFTING_ICON = "./images/craftingIcon.png";
