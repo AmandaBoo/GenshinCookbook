35aed95060401d6be017878d152c7244bc39cf6e
Amanda Bui <buixx199@umn.edu>
2021-07-22 23:55:05 -0500

    Converts JSON files used for server side storage into js objects; cleans up file system Closes #6

--- jsonInterfaces/serverSideInterface.js
+++ jsonInterfaces/serverSideInterface.js
@@ -1,11 +1,15 @@
+import {RAW_INGREDIENTS} from "../storage/serverSideStorageTemplates/rawIngredientsDescription.js";
+import {FOOD_INGREDIENTS} from "../storage/serverSideStorageTemplates/foodIngredientsDescription.js";
+import {FOOD_RECIPES} from "../storage/serverSideStorageTemplates/foodRecipesDescription.js";
+
 export function getRawIngredientsFromLocalStorage() {
-    return rawIngredientsJson.rawIngredients;
+    return RAW_INGREDIENTS.rawIngredients;
 }
 
 export function getFoodIngredientsFromLocalStorage() {
-    return foodIngredientsJson.foodIngredients;
+    return FOOD_INGREDIENTS.foodIngredients;
 }
 
 export function getFoodRecipesFromLocalStorage() {
-    return foodRecipeJson.foodRecipes;
+    return FOOD_RECIPES.foodRecipes;
 }
