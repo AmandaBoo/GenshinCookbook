006533f5350d424254cac5831f06ff80df083d48
Amanda Bui <buixx199@umn.edu>
2021-07-24 17:33:07 -0500

    #10 - adds tracking for a user's desired quantity, desire for mastery, and user's current proficiency with FoodRecipe object in local storage - updates interface to handle new fields - adds recipe images and cooking icon

--- storage/localStorageTemplates/jsTemplates/foodRecipesTemplate.js
+++ storage/localStorageTemplates/jsTemplates/foodRecipesTemplate.js
@@ -1,10 +1,14 @@
 export const FOOD_RECIPES_TEMPLATE = "[{\n" +
     "  \"name\": \"Steak\",\n" +
     "  \"qty\": 0,\n" +
-    "  \"want\": 0\n" +
+    "  \"want\": 0,\n" +
+    "  \"mastery\": false,\n" +
+    "  \"curProf\": 0\n" +
     "},\n" +
     "{\n" +
     "  \"name\": \"Triple-Layered Consommé\",\n" +
     "  \"qty\": 0,\n" +
-    "  \"want\": 0\n" +
+    "  \"want\": 0,\n" +
+    "  \"mastery\": false,\n" +
+    "  \"curProf\": 0\n" +
     "}]";
