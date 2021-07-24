bffe68b84297c6af6c32555a23d8863440ac5609
OskarAlyn <alyn0001@umn.edu>
2021-07-23 20:16:43 -0500

    Fix ingredient image file paths

--- storage/serverSideStorageTemplates/rawIngredientsDescription.js
+++ storage/serverSideStorageTemplates/rawIngredientsDescription.js
@@ -2,27 +2,27 @@ export const RAW_INGREDIENTS = {
     rawIngredients: [
         {
             name: "Fowl",
-            src: "../images/fowl.png"
+            src: "./images/fowl.png"
         },
         {
             name: "Bamboo Shoot",
-            src: "../images/bambooShoot.png"
+            src: "./images/bambooShoot.png"
         },
         {
             name: "Mushroom",
-            src: "../images/mushroom.png"
+            src: "./images/mushroom.png"
         },
         {
             name: "Raw Meat",
-            src: "../images/rawMeat.png"
+            src: "./images/rawMeat.png"
         },
         {
             name: "Milk",
-            src: "../images/milk.png"
+            src: "./images/milk.png"
         },
         {
             name: "Salt",
-            src: "../images/salt.png"
+            src: "./images/salt.png"
         }
     ]
 }
