8101f4c88c159ef40018ccc30df0c96f7c1a1e61
Joseph O'Sullivan <osull064@umn.edu>
2021-09-15 22:48:13 -0500

    #36 Removed Comments

--- src/components/shared/Icon.js
+++ src/components/shared/Icon.js
@@ -1,28 +1,12 @@
-// import React, { Component } from 'react';
-
-// // TODO : CONVERT TO FUNCTIONAL COMPONENT
-// export class Icon extends Component {
-//     render() {
-//         return (
-//             <button
-//                 className={"btn"}
-//                 onClick={() => this.props.onClick(this.props.id)}
-//             >
-//                 {this.props.text}
-//             </button>
-//         )
-//     }
-// }
 import React from 'react';
 
-// TODO : CONVERT TO FUNCTIONAL COMPONENT
 export function Icon(props) {    
     return (
         <button
             className={"btn"}
             onClick={() => props.onClick(props.id)} 
         >
-         {props.text}
+        {props.text}
         </button>
     )  
 }
--- src/components/shared/NavBarTab.js
+++ src/components/shared/NavBarTab.js
@@ -1,50 +1,6 @@
-// import React, { Component } from 'react';
-
-// // TODO : CONVERT INTO FUNCTIONAL COMPONENT
-// export class NavBarTab extends Component {
-//     /* PROPS
-//     * id, ?src, ?name, selectedTab
-//     * */
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-//         if (this.props.src !== undefined) {
-//             return (
-//                 <img
-//                     className={
-//                         `inventory-nav-bar-tab-icon 
-//                         ${this.props.isSelected ? "selected-tab" : ""}`
-//                     }
-//                     id={this.props.id}
-//                     src={this.props.src}
-//                     alt={this.props.src}
-//                     onClick={() => this.props.onClick(this.props.id)}
-//                 />
-//             );
-//         } else if (this.props.name !== undefined) {
-//             return (
-//                 <label
-//                     className={"site-nav-bar-links"}
-//                     id={this.props.id}
-//                 >
-//                     this.props.name
-//                 </label>
-//             );
-//         }
-//     }
-// }
 import React from 'react';
 
-// TODO : CONVERT INTO FUNCTIONAL COMPONENT
 export function NavBarTab (props){
-    /* PROPS
-    * id, ?src, ?name, selectedTab
-    * */
-    //constructor(props) {
-    //    super(props);
-    //}
     if (props.src !== undefined) {
        return (
             <img
