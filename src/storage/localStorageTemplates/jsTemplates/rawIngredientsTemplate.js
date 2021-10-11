17705810f684d0ddb8a85cdfe775869c39c55665
Amanda Bui <buixx199@umn.edu>
2021-10-11 00:06:55 -0500

    #65 - Update storage - adds images for all raw and crafted ingredients - updates server and local templates with new crafted and raw ingredients - moves non-cooking related images

--- src/storage/localStorageTemplates/jsTemplates/rawIngredientsTemplate.js
+++ src/storage/localStorageTemplates/jsTemplates/rawIngredientsTemplate.js
@@ -1,22 +1,154 @@
 export const RAW_INGREDIENTS_TEMPLATE = "[\n" +
     "  {\n" +
-    "    \"name\": \"Fowl\",\n" +
-    "    \"qty\": 0\n" +
+    "    \"name\":\"Almond\",\n" +
+    "    \"qty\":0\n" +
     "  },\n" +
     "  {\n" +
-    "    \"name\": \"Bamboo Shoot\",\n" +
-    "    \"qty\": 0\n" +
+    "    \"name\":\"Apple\",\n" +
+    "    \"qty\":0\n" +
     "  },\n" +
     "  {\n" +
-    "    \"name\": \"Mushroom\",\n" +
-    "    \"qty\": 0\n" +
+    "    \"name\":\"Bamboo Shoot\",\n" +
+    "    \"qty\":0\n" +
     "  },\n" +
     "  {\n" +
-    "    \"name\": \"Milk\",\n" +
-    "    \"qty\": 0\n" +
+    "    \"name\":\"Berry\",\n" +
+    "    \"qty\":0\n" +
     "  },\n" +
     "  {\n" +
-    "    \"name\": \"Salt\",\n" +
-    "    \"qty\": 0\n" +
+    "    \"name\":\"Bird Egg\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Cabbage\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Calla Lily\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Carrot\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Chilled Meat\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Crab\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Fowl\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Horsetail\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Jueyun Chili\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Lavender Melon\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Lotus Head\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Matsutake\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Mint\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Mushroom\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Onion\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Pepper\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Pinecone\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Potato\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Qingxin\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Radish\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Rice\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Sakura Bloom\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Salt\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Seagrass\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Shrimp Meat\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Small Lamp Grass\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Snapdragon\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Sunsettia\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Sweet Flower\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Tofu\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Tomato\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Unagi Meat\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Violetgrass\",\n" +
+    "    \"qty\":0\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"name\":\"Wheat\",\n" +
+    "    \"qty\":0\n" +
     "  }\n" +
     "]\n";
